b21427686a647e48b7d568c54a2e342472e2140c
camila <cm.maschion@gmail.com>
2022-10-12 20:06:15 +0100

    view binding + splash screen

--- app/src/main/java/com/maschion/camisaria/MainActivity.kt
+++ app/src/main/java/com/maschion/camisaria/MainActivity.kt
@@ -2,20 +2,31 @@ package com.maschion.camisaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.maschion.camisaria.adapter.CustomAdapter
 import com.maschion.camisaria.data.model.ClientList
+import com.maschion.camisaria.databinding.ActivityMainBinding
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val installSplashScreen = installSplashScreen()
+//        installSplashScreen.setKeepOnScreenCondition(SplashScreen.KeepOnScreenCondition {
+//            true
+//        })
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // getting the recyclerview by its id
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+        val recyclerview = binding.recyclerview
 
         // this creates a vertical layout Manager
         recyclerview.layoutManager = LinearLayoutManager(this)
@@ -24,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         val data = ArrayList<ClientList>()
 
         for (i in 1..20) {
-            data.add(ClientList(name = "Camila", date = Date(10/10/1100), local = "Sao Paulo"))
+            data.add(ClientList(name = "Leo Onardo", date = Date(10/10/1100), local = "Sao Paulo"))
         }
 
         // This will pass the ArrayList to our Adapter
